01059a6d6ad701b9529fd28130a48d80f25d1151
strucom <140485265+strucom@users.noreply.github.com>
2025-07-02 14:53:24 +0400

    Create FileSystemException.php

---
+++ src/FileSystemException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Strucom\Exception;
+use Exception;
+
+/**
+ * Exception for file system errors
+ *
+ * @author Albrecht Fischer
+ * @version 1.0
+ */
+class FileSystemException extends Exception
+{
+
+}
