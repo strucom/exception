8c9119ffe7be10361cba91356cf37cfb9db5a03a
strucom <140485265+strucom@users.noreply.github.com>
2025-07-03 09:07:10 +0400

    Create DatabaseException.php

---
+++ src/DatabaseException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Strucom\Exception;
+use Exception;
+
+/**
+ * Exception for file system errors
+ *
+ * @since PHP 5.0
+ * @author af
+ */
+class DatabaseException extends Exception
+{
+
+}
