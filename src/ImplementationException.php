d5bcccf68f27d2af47d330deaf2ab5eea29f92aa
strucom <140485265+strucom@users.noreply.github.com>
2025-07-07 12:15:12 +0400

    Create ImplementationException.php

---
+++ src/ImplementationException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Strucom\Exception;
+use Exception;
+
+/**
+ * Exception for implementation errors
+ *
+ * @since PHP 5.0
+ * @author af
+ */
+class ImplementationException extends Exception
+{
+
+}
